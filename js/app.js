b3ec9409fe7f76264be7882136558ac948cb4295
mohammed dhedy <mhmddhedy1234@gmail.com>
2024-10-21 11:10:45 +0300

    add the movie detail page to review movies

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// const apiKey = 'YOUR_API_KEY';
+// const url = `https://api.themoviedb.org/3/movie/popular?api_key=${'6345300bfd3be8919424dc0b3e207528'}&language=en-US&page=1`;
+
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => console.log(data.results))
+//   .catch(error => console.error('Error:', error));
